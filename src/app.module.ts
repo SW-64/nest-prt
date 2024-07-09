cfcd0127284f91dd82e1db488a573123200ba855
SW-64 <ettjddns6@gmail.com>
2024-07-09 21:05:44 +0900

    feat : TypeOrm을 이용한 데이터베이스 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config'; // 환경변수 관리 및 환경변수 유효성 검사
-import Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmModuleOptions } from './config/database.config';
+import { configModuleValidationSchema } from './config/env-validation.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      validationSchema: Joi.object({
-        SERVER_PORT: Joi.number().required(),
-      }),
+      validationSchema: configModuleValidationSchema,
     }),
+    TypeOrmModule.forRootAsync(typeOrmModuleOptions),
   ],
   controllers: [AppController],
   providers: [AppService],
